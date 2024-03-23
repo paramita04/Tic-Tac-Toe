bd718662b3d3a7a9be2b81348910a84ff4bb5aa3
Paramita Sen <147388408+paramita04@users.noreply.github.com>
2024-03-24 01:44:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let boxes=document.querySelectorAll(".box");
-let resetBtn=document.querySelector("#reset-btn");
-let newGameBtn=document.querySelector("#new-btn");
-let msgContainer=document.querySelector(".msg-container");
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
-let turnO=true;
-const winPatterns=[
+let turnO = true;
+const winPatterns = [
     [0,1,2],
     [0,3,6],
     [0,4,8],
@@ -12,24 +12,25 @@ const winPatterns=[
     [2,5,8],
     [2,4,6],
     [3,4,5],
-    [6,7,8],
+    [6,7,8]
 ];
-const resetGame=()=>{
-    turnO=true;
+const resetGame = () => {
+    turnO = true;
     enableBoxes();
     msgContainer.classList.add("hide");
 };
-boxes.forEach((box) => {
+boxes.forEach ((box) => {
     box.addEventListener("click",() =>{
-        console.log("box was clicked");
-        if(turnO) {
+        
+        if(turnO == true) {
             box.innerText="O";
-            turnO=false;
-        } else {
-            box.innerText="X";
-            turnO=true;
+            turnO = false;
+        } 
+        else {
+            box.innerText = "X";
+            turnO = true;
         }
-        box.disabled=true;
+        box.disabled = true;
         checkWinner();
     });
 });
@@ -40,10 +41,10 @@ const disableBoxes = ()=>{
     }
 };
 
-const enableBoxes=()=>{
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
     }
 };
 const showWinner = (winner) =>{
@@ -53,13 +54,12 @@ const showWinner = (winner) =>{
 };
 const checkWinner = () => {
     for(pattern of winPatterns){
-        // console.log(pattern[0],pattern[1],pattern[2]);
-        // console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
+        
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
         if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
-            if(pos2Val===pos2Val && pos2Val===pos3Val){
+            if(pos1Val===pos2Val && pos2Val===pos3Val){
                 console.log("Winner",pos1Val);
                 showWinner(pos1Val);
             }
